4d7e5f081c70de721d5e7dafbc250b70ed2302c3
Jason Hess <Jason.Hess@michaelhill.com.au>
2019-01-31 14:07:11 +1000

    add images

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -6,12 +6,39 @@ import { Component } from '@angular/core';
 })
 export class ProductListComponent {
     pageTitle = 'Product List';
+    imageWidth = 50;
+    imageMargin = 2;
     products: Product[] = [
-        { name: 'Product 1' },
-        { name: 'Product 2' }
+        {
+            productId: 1,
+            productName: "Leaf Rake",
+            productCode: "GDN-0011",
+            releaseDate: "March 19, 2016",
+            description: "Leaf rake with 48-inch wooden handle.",
+            price: 19.95,
+            starRating: 3.2,
+            imageUrl: "https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+        },
+        {
+            "productId": 2,
+            "productName": "Garden Cart",
+            "productCode": "GDN-0023",
+            "releaseDate": "March 18, 2016",
+            "description": "15 gallon capacity rolling garden cart",
+            "price": 32.99,
+            "starRating": 4.2,
+            "imageUrl": "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
+        }
     ]
 }
 
 class Product {
-    name: string = 'Product Name 123';
+    productId: number;
+    productName: string;
+    productCode: string;
+    releaseDate: string;
+    description: string;
+    price: number;
+    starRating: number;
+    imageUrl: string;
 }
