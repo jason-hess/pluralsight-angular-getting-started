62c1f668bb8943242e09b58ddae49c9db219a556
Jason Hess <Jason.Hess@michaelhill.com.au>
2019-02-08 08:59:38 +1000

    add routing

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
@@ -9,6 +10,7 @@ import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star.component';
 import { ProductDetailComponent } from './products/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
+
 
 @NgModule({
   declarations: [
@@ -20,7 +22,17 @@ import { WelcomeComponent } from './home/welcome.component';
     WelcomeComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule, FormsModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(
+      [
+        { path: 'products', component: ProductListComponent },
+        { path: 'product/:id', component: ProductDetailComponent },
+        { path: 'welcome', component: WelcomeComponent },
+        { path: '', redirectTo: 'welcome', pathMatch: 'full' }
+        // { path: '**', component: PageNotFoundComponent },
+      ])
   ],
   bootstrap: [AppComponent]
 })
